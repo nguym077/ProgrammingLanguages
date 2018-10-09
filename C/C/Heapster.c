42ecd1a9a862d7004138cd6cc4bb76bed098ae4b
Marian Nguyen <itsmariann@gmail.com>
2018-10-08 19:49:38 -0700

    initial commit

---
+++ C/C/Heapster.c
@@ -0,0 +1,13 @@
+// C.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int main()
+{
+	// printf() displays the string inside quotation
+	printf("Hello, dd World!");
+	return 0;
+}
+
